7726dd8c0ff065eb98745c672e3066839274a38a
Timpie90 <oeoeau@hotmail.nl>
2017-11-16 19:06:26 +0100

    Css en js ook in een include ding

--- Registration.php
+++ Registration.php
@@ -6,15 +6,15 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-	<link rel="stylesheet" href="css/testing.css">
+    
   </head>
   <body>
     
   		<?php
   			include "files/DBConnection.php";
 			include "files/Navbar.php";
+			include "files/Css.php";
+
   			$username = $usernameErr = "";
   			$password = $passwordErr = "";
   			$confirmPassword = $confirmPasswordErr = "";

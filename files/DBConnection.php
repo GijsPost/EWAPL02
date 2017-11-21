7b1e39c6e7878aaf5121168975290f5610adb2e2
GijsGozer <gijspost456@gmail.com>
2017-11-21 10:18:42 +0100

    Navbar en DBConnection in /files gezet, probeer de home-folder voor webpages te houden

--- files/DBConnection.php
+++
@@ -1,5 +0,0 @@
-
-<?php
-$db = new PDO("mysql:host=h2713427.stratoserver.net;dbname=ewapl02", "ewapl02", "ewapl02");
-//"mysql:host=h2713427.stratoserver.net;dbname=ewapl02", "ewapl02", "ewapl02"
-?>

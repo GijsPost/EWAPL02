1c86d4671a80885ad7569599d9d43a0e1c33c378
Tim Vlaar <tim.vlaar@hva.nl>
2017-12-05 14:36:25 +0100

    admin

--- admin/AdminOverview.php
+++ admin/AdminOverview.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <title>Empty</title>
+    <title>Admin Overview</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -11,19 +11,32 @@
   <body>
     
   	<?php
-      include "files/DBConnection.php";
-      include "files/Css.php";
+      include "../files/DBConnection.php";
+      include "../files/Css.php";
     ?>
 
-    
-      <div class="col-md-6">
-        <div style = "margin-top: 40px; margin-bottom: 40px;">
-          <hr>
-            <center><h1>Empty</h1></center>
-          <hr>
-        </div>    
+    <div class="col-md">
+        <hr>
+          <center><h3>Admin overview</h3></center>
+        <hr>
+    </div>
+
+    <div class="row">
+      <div class="col-md">
+        <a href="AdminUser.php">Users</a>    
       </div>
-    
+    </div>
+    <div class="row">
+      <div class="col-md">  
+        <a href="AdminArticle.php">Articles</a>
+      </div>
+    </div>  
+    <div class="row">
+      <div class="col-md">  
+        <a href="AdminComment.php">Comments</a>
+      </div>
+    </div>    
+  
 
   
   </body>
